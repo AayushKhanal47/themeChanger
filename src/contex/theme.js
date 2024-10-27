8d049c1b414f90ebf62df6d8e28e4e4b1555b526
AayushKhanal47 <aayushkhanal810@gmail.com>
2024-10-28 00:35:04 +0530

    react

---
+++ src/contex/theme.js
@@ -0,0 +1,15 @@
+import { useContext, createContext } from "react";
+
+export  const ThemeContex = createContext({
+    themeMode: "light",
+    darkTheme: ()=>{},
+    lightTheme: ()=>{}
+})
+
+export const ThemeProvider = ThemeContex.Provider
+
+export default function useTheme(){
+    return useContext(ThemeContex)
+}
+
+
